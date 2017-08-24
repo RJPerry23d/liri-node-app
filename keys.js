ca101fe94a7ae5664663e84523ec4e2ca2882565
RJ <rj@blacklambs.com>
2017-08-24 14:20:19 -0400

    adding all setup files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<MawP9LqXAKcQIDYLBrcLsJyGa>',
+  consumer_secret: '<MqcyQWq79S7SwjKAnwoiwHPur5WNstmziZhg5iz2BCpE2eY7Pf>',
+  access_token_key: '<267519448-KYi9NcU0XjEpqo8Hpc0LRrPmDymYCvIZavrcLJeL>',
+  access_token_secret: '<BppswDK00zptyaPlJ1xyvoq5h7uzeLmFxxkAC4t5dRhhW>',
+}
